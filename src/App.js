52c452bdb5236f37f124cebd337ba1f73c1bdd69
Akashdeep Patra <adeep8961@gmail.com>
2020-09-30 03:17:55 +0530

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.scss";
-import HomePage from "./Components/homepage.component";
+import HomePage from "./Pages/homepage.component";
 export default function App() {
   return (
     <div className="App">
--- src/Pages/homepage.component.js
+++ src/Pages/homepage.component.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./homepage.component.scss";
 
 export default (HomePage) => (
