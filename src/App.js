38609af3700489f96b262a52e2b8f023e5e1bb1d
Akashdeep Patra <adeep8961@gmail.com>
2020-09-30 03:02:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.scss";
+import HomePage from "./Components/homepage.component";
+export default function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
