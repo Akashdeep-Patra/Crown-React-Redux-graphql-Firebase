eb868120b621ee2515cc8c112d8369dec6efb3ea
Akashdeep Patra <adeep8961@gmail.com>
2020-10-01 01:41:11 +0530

    header section and routing controll added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./index.scss";
 import HomePage from "./Pages/homepage/homepage.component";
 import ShopPage from "./Pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
+import Header from "./Components/header/header";
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/Components/header/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./header.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+export default (props) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
+    </div>
+  </div>
+);
