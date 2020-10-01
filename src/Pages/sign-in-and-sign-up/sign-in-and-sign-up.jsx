702d74a33f1e2ac33c81515d6b290eaafd4a6c24
Akashdeep-Patra <adeep8961@gmail.com>
2020-10-01 14:03:45 +0000

    added

--- src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./sign-in-and-sign-up.scss";
 import SignIn from "../../Components/signin/signin";
-import FormInput from "../../Components/form-input/form-input";
+
 export default (props) => (
-  <div className="sign-in-and-sign-up">
-    <SignIn />
-  </div>
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
 );
