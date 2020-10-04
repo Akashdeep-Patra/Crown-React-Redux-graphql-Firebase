3c9bf346e2b8e95a8ad5cc290a98257e61d5f7e2
Akashdeep-Patra <adeep8961@gmail.com>
2020-10-04 22:33:48 +0000

    Updates

---
+++ precache-manifest.de89acae36586d2008447429d733e892.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a4aa1d73262e2ecae5530c17151d3d",
+    "url": "/Crown-React-Redux-graphql-Firebase/index.html"
+  },
+  {
+    "revision": "5736ed15f888022c8a25",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/css/main.0ea21c03.chunk.css"
+  },
+  {
+    "revision": "2f7b326d21e5faeb4676",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/js/2.28bf11a7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/js/2.28bf11a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5736ed15f888022c8a25",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/js/main.5fb2b0f4.chunk.js"
+  },
+  {
+    "revision": "7065a50f952aaa5d1a6e",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/js/runtime-main.2ee4ba97.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Crown-React-Redux-graphql-Firebase/static/media/shopping-bag.30f07071.svg"
+  }
+]);
