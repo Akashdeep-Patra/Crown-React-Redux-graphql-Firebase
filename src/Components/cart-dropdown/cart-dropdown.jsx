64a40b6b49a07eed2f755498d9b2eafca7c040a7
lucifer <adeep8961@gmail.com>
2020-10-19 00:13:20 +0530

    used reselect to build selectors in acrt

--- src/Components/cart-dropdown/cart-dropdown.jsx
+++ src/Components/cart-dropdown/cart-dropdown.jsx
@@ -3,7 +3,7 @@ import "./cart-dropdown.scss";
 import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button";
 import CartItem from "../cart-item/CartItem.component";
-
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 const cart = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
@@ -15,7 +15,7 @@ const cart = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 export default connect(mapStateToProps, null)(cart);
--- src/Components/cart-icon/cart-icon.jsx
+++ src/Components/cart-icon/cart-icon.jsx
@@ -3,10 +3,11 @@ import "./cart-icon.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggelCartHidden } from "../../redux/cart/cart.actions";
-const CartIcon = ({ toggelCartHidden }) => (
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+const CartIcon = ({ toggelCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggelCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -14,4 +15,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggelCartHidden: () => dispatch(toggelCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
