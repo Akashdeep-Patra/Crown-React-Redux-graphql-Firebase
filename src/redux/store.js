8b5a8ed760071c37bdba8275e156e22c816d5dfe
Akashdeep <adeep8961@gmail.com>
2020-10-26 00:52:17 +0530

    production logs removed from middleware redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const middleware = [logger];
+const middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 const persistor = persistStore(store);
 export { store, persistor };
