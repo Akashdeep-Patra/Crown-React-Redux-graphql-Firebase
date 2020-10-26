605a21ea612590f3623aa8470ffe29e65b20a799
Akashdeep <adeep8961@gmail.com>
2020-10-27 02:14:35 +0530

    implemented code that pulls in shop data from fs

--- src/Pages/shop/shop.component.jsx
+++ src/Pages/shop/shop.component.jsx
@@ -1,13 +1,65 @@
 import React from "react";
 import "./shop.component.scss";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop.actions";
 import CollectionsOverView from "../../Components/collections-overview/CollectionsOverView";
 import Collection from "../collection/Collection.component";
-const Shop = ({ match }) => (
-  <div className="shoppage">
-    <Route exact path={`${match.path}`} component={CollectionsOverView} />
-    <Route path={`${match.path}/:collectionId`} component={Collection} />
-  </div>
-);
+// import axios from "axios";
+import {
+  firestore,
+  convertCollectionsSnapShotToMap,
+} from "../../firebase/firebase.utils";
+import WithSpinner from "../../Components/with-spinner/with-spinner.component";
+const CollectionsOverViewWithSpinner = WithSpinner(CollectionsOverView);
+const CollectionWithSpinner = WithSpinner(Collection);
 
-export default Shop;
+class Shop extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  async componentDidMount() {
+    const { updateCollections } = this.props;
+    // const dbResponse = axios.get(
+    //   "https://firestore.googleapis.com/v1/projects/crown-db-d3bdf/databases/(default)/documents/collections"
+    // );
+    // console.log(dbResponse);
+    const collectionRef = firestore.collection("collections");
+    const snapShot = await collectionRef.get();
+    const collectionsMap = await convertCollectionsSnapShotToMap(snapShot);
+    updateCollections(collectionsMap);
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shoppage">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverViewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => (
+            <CollectionWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(Shop);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -56,4 +56,35 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapShotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollections.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export default firebase;
