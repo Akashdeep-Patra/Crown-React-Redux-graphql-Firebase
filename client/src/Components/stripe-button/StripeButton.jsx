35322fe63bc91359e16b2291f5d5c51943830daa
Akashdeep <adeep8961@gmail.com>
2020-11-04 00:25:54 +0530

    full stack

--- client/src/Components/stripe-button/StripeButton.jsx
+++ client/src/Components/stripe-button/StripeButton.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import "./StripeButton.scss";
-
+import axios from "axios";
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51Hf0SYADaYzpNAWBh1bBMa4eNyZYZcfUEz2gsvtMYfw9WwruYGYxKMpR7nlzasqXgOXo0rBPE3sbXmlPZEqZN4dQ00EbzPr2pP";
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    // console.log(token);
+
+    axios({
+      url: "/payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful");
+      })
+      .catch((error) => {
+        // console.log("Payment error: ", JSON.parse(error));
+        console.log(error);
+        // console.log("payments error");
+        alert("There was an issue with your payment");
+      });
   };
   return (
     <StripeCheckout
