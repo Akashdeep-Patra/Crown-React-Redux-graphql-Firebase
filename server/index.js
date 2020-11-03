c5cd86ed1f00db7770cae7fe1465a77e225b9a7f
Akashdeep <adeep8961@gmail.com>
2020-11-04 02:06:19 +0530

    temp

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 app.listen(port, (error) => {
