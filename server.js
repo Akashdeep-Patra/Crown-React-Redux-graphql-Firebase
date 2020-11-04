6767dcbda5748ea08e182057f12f8de99c88a53e
Akashdeep <adeep8961@gmail.com>
2020-11-04 23:17:59 +0530

    modifyied

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
+
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("Server is running on port " + port);
+  console.log("Server running on port " + port);
 });
 
 app.post("/payment", (req, res) => {
@@ -30,12 +34,12 @@ app.post("/payment", (req, res) => {
     amount: req.body.amount,
     currency: "usd",
   };
-  stripe.charges.create(body, (stripeError, stripeResponse) => {
-    if (stripeError) {
-      console.log(stripeError);
-      res.status(500).send({ error: stripeError });
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
     } else {
-      res.status(200).send({ success: stripeResponse });
+      res.status(200).send({ success: stripeRes });
     }
   });
 });
