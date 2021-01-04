999cebbc6cfd780a9677169cdd0ccfe780b7bfc1
Akashdeep-Patra <adeep8961@gmail.com>
2021-01-04 16:41:22 +0000

    scripts for pipeline added

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", function (req, res) {
+  app.get("*",  (req, res)=> {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
-}
+
 
 app.listen(port, (error) => {
   if (error) throw error;
